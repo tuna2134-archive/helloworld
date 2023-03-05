8e9a0db3b9351e16860ff4ee6ae1dfede265995d
tuna2134 <tuna2134@noreply.github.com>
2023-03-05 22:16:43 +0900

    fix: typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
       <div className="px-16 py-32 text-white bg-gradient-to-r from-sky-500 to-indigo-700">
         <h1 className="text-4xl">Hello, World!</h1>
         <p className="mt-6">それはエンジニアたちが必ず目にする言葉</p>
-        <p>このサイトは初心者にプログラミングを学習しながら、楽しくするためのサイトです。</p>
+        <p>このサイトは初心者がプログラミングを学習しながら、楽しくするためのサイトです。</p>
       </div>
     </>
   )
