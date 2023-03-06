a8785946ea1280119cb1ec42d2081833c3214998
tuna2134 <tuna2134@noreply.github.com>
2023-03-06 15:44:09 +0900

    iroiro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import NextHeadSeo from 'next-head-seo';
 
+import axios from 'axios';
 
-export default function Index() {
+
+type Article = {
+  title: string,
+  description: string,
+  author_id: number,
+};
+
+type Props = {
+  articles: Article[];
+};
+
+export default function Index({ articles }: Props) {
   return (
     <>
       <NextHeadSeo 
@@ -16,13 +28,24 @@ export default function Index() {
       </div>
       <div className="mt-8">
         <h2 className="text-3xl text-center">記事一覧</h2>
-        <div className="mx-[15%]">
-          <div>
-            <p className="text-2xl font-semibold">ハロー</p>
-            <small>概要だよ</small>
+        {articles.map((article: Article) => {
+          <div className="mx-[15%]">
+            <div className="border-2 rounded-md px-2 py-2 space-y-4">
+              <p className="text-2xl font-semibold">{article.title}</p>
+              <small>{article.description}</small>
+            </div>
           </div>
-        </div>
+        })}
       </div>
     </>
-  )
-}
+  );
+};
+
+export async function getServerSideProps() {
+  const req = await axios.get(`${process.env.BACKEND_URL}/api/articles`);
+  return {
+    props: {
+      articles: req.data,
+    },
+  };
+};
